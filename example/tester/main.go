824d8bf2e11da1628c53cc89420f7d2ef6d06392
David Betts <wbetts@twilio.com>
2022-10-13 09:14:31 -0600

    Per https://github.com/golang/go/issues/45604 create buffered channel for signal.Notify

--- example/tester/main.go
+++ example/tester/main.go
@@ -82,7 +82,7 @@ func main() {
 		errc <- server.ListenAndServe()
 	}()
 
-	sigchan := make(chan os.Signal)
+	sigchan := make(chan os.Signal, 1)
 	signal.Notify(sigchan, syscall.SIGINT, syscall.SIGTERM)
 
 	select {
